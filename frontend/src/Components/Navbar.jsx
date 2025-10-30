d5ddfd6ee35d5e0623485a2e3fb0cef44cc2686a
HarshilVora <harshilvora8160@gmail.com>
2025-10-30 23:17:09 +0530

    running create navbar

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <div className="bg-gray-200 px-30" >
+            <div>
+                <span className="flex" >Navbar</span>
+            </div>
+            <div className="flex justify-between" >
+                <span>Navbar</span>
+                <span className="" >Navbar</span>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
